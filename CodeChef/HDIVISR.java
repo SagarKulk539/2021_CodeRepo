ee176b70ef23bbf661e045f11c83914284dd1961
Sagar Kulkarni <44294928+SagarKulk539@users.noreply.github.com>
2021-04-07 19:51:22 +0530

    First Commit

---
+++ CodeChef/HDIVISR.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class HDIVISR
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        for(int i=10;i>=1;i--)
+        {
+            if(n%i==0)
+            {
+                System.out.println(i);
+                break;
+            }
+        }
+        sc.close();
+	}
+}

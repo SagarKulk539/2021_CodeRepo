92bd0a02d41dbf6a4a4ec8c5d1d21f42395efc5b
Sagar Kulkarni <44294928+SagarKulk539@users.noreply.github.com>
2021-04-07 19:47:34 +0530

    First Commit

---
+++ CodeChef/SOCKS1.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class SOCKS1
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+          Scanner sc=new Scanner(System.in);
+          int a,b,c;
+          a=sc.nextInt();
+          b=sc.nextInt();
+          c=sc.nextInt();
+
+          int[] countArray=new int[11];
+          countArray[a]++;
+          countArray[b]++;
+          countArray[c]++;
+
+          int flag=0;
+          for(int i=1;i<=10;i++)
+          {
+              if(countArray[i]>1)
+              {
+                  flag=1;
+                  break;
+              }
+          }
+
+          if(flag==0)
+              System.out.println("NO");
+          else
+              System.out.println("YES");
+
+          sc.close();
+    }
+}
